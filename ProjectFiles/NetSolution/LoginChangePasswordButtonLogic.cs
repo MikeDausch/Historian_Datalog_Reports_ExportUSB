eb93dc2d21b8a9e120bcb04c88f66a5bc498b5e3
MikeDausch <Mike.Dausch@gmail.com>
2024-09-26 14:00:24 -0500

    Inital Commit

---
+++ ProjectFiles/NetSolution/LoginChangePasswordButtonLogic.cs
@@ -0,0 +1,60 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.NetLogic;
+using FTOptix.NativeUI;
+using FTOptix.HMIProject;
+using FTOptix.UI;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.Retentivity;
+using FTOptix.Report;
+using FTOptix.RAEtherNetIP;
+using FTOptix.ODBCStore;
+using FTOptix.InfluxDBStore;
+using FTOptix.CommunicationDriver;
+using FTOptix.MicroController;
+using FTOptix.Modbus;
+using FTOptix.System;
+#endregion
+
+public class LoginChangePasswordButtonLogic : BaseNetLogic
+{
+    [ExportMethod]
+    public void PerformChangePassword(string oldPassword, string newPassword, string confirmPassword)
+    {
+        var outputMessageLabel = Owner.Owner.GetObject("ChangePasswordFormOutputMessage");
+        var outputMessageLogic = outputMessageLabel.GetObject("LoginChangePasswordFormOutputMessageLogic");
+
+        if (newPassword != confirmPassword)
+        {
+            outputMessageLogic.ExecuteMethod("SetOutputMessage", new object[] { 7 });
+        }
+        else
+        {
+            var username = Session.User.BrowseName;
+            try
+            {
+                var userWithExpiredPassword = Owner.GetAlias("UserWithExpiredPassword");
+                if (userWithExpiredPassword != null)
+                    username = userWithExpiredPassword.BrowseName;
+            }
+            catch
+            {
+            }
+
+            var result = Session.ChangePassword(username, newPassword, oldPassword);
+            if (result.ResultCode == ChangePasswordResultCode.Success)
+            {
+                var parentDialog = Owner.Owner?.Owner?.Owner as Dialog;
+                if (parentDialog != null && result.Success)
+                    parentDialog.Close();
+            }
+            else
+            {
+                outputMessageLogic.ExecuteMethod("SetOutputMessage", new object[] { (int)result.ResultCode });
+            }
+        }
+    }
+}
